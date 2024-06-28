b8dbeaadcb0f36674b825bd9000632762dd7f17c
Sebastian Kuipers <kuipers.sebastian@ziggo.nl>
2024-06-28 14:29:59 +0200

    fixed continueing on read messages

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -244,7 +244,7 @@ class Reddit(commands.Cog):
 
         async for conv in new_conversations:
             if conv.last_unread is None:
-                continue
+                return
             embed = await self.create_modmail_embed(conv)
             await self.bot.modmail_channel.send(embed=embed)
             await conv.read()
@@ -254,7 +254,7 @@ class Reddit(commands.Cog):
         ] = self.subreddit.modmail.conversations(sort="unread", state="appeals")
         async for conv in appeals_conversations:
             if conv.last_unread is None:
-                continue
+                return
             embed = await self.create_modmail_embed(conv)
             await self.bot.modmail_channel.send(embed=embed)
             await conv.read()
